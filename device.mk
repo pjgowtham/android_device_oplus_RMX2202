610057d3b1745995e99863ab051e65868f946862
pjgowtham <pjgowtham@gmail.com>
2022-03-12 12:41:44 +0530

    RMX3360: Update mixer_paths from stock RUI3 for both GT,GT-ME

--- device.mk
+++ device.mk
@@ -40,8 +40,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml
-
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_yupikidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_yupik/mixer_paths_yupikidp.xml
+    
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
